47fb7f55ffe890a67c960f34e3097f0fa98731c1
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-10-01 16:27:11 -0700

    commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height, // Full screen height
+          width: MediaQuery.of(context).size.width, // Full screen width
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.purple, Colors.purple.shade300, Colors.yellow],
+            ),
+          ),
+          child: Center(
+            // Centering content inside the container
+            child: Text(
+              'Splash Screen',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
